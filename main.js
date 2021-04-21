d6a5790ee305756858d6ca74b9250a82789d2a9c
Thomas Miller <thomas.miller147@gmail.com>
2021-04-21 20:07:12 +0200

    fix

--- main.js
+++ main.js
@@ -1,19 +1,14 @@
 import "./style.css";
 
-// document.querySelector("#app").innerHTML = `
-//   <h1>Vite app</h1>
-// `;
 var frameNumber = 0;
 var playbackConst = 500;
 var bodyHeight = document.getElementById("body");
 var vid = document.getElementById("rose-video");
 
-// dynamically set the page height according to video length
 vid.addEventListener("loadedmetadata", function () {
   bodyHeight.style.height = Math.floor(vid.duration) * playbackConst + "px";
 });
 
-// Use requestAnimationFrame for smooth playback
 function scrollPlay() {
   var frameNumber = window.pageYOffset / playbackConst;
   vid.currentTime = frameNumber;
